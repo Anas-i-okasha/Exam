6dbd18f822356e99560c88b72a8e6b039414586c
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-01-01 21:47:37 +0200

    solve q1

---
+++ part_1/library-app/back-end/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'0000',
+    database: 'books',
+
+  });
+
+  connection.connect((err)=>{
+      if(err) throw err
+
+      console.log('Connected db')
+  })
+
+  // create database books (id INT NOT)
+   
