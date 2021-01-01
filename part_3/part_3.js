f13e4115febe2161c66fd8437c15bfc08be42ba7
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-01-01 22:32:58 +0200

    internet interrupt

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -6,31 +6,53 @@ Type the correct answer below each question:
 
 Q1) Why do we use joins in SQL?
 
+// when we use relation database 
+
 
 Q2) Name three types of relationships in databases.
+
+1- one to one 
+2- one to many 
+3- many to many 
 
 
 Q3) What are the main three sections of a JWT?
 
+body , seginuter , head 
+
 
 Q4) Name three types of express middle-wares.
 
+error middle ware , router  , buitl in middle watre
+
+
 
 Q5) What is the `effect` hook used for in react?
+// similar to compnentDidMount  to update the the web api 
+ 
 
 
 Q6) What is the `state` hook used for in react?
 
+to save the data recived from database it will be changed in the future 
+
 
 Q7) List three thing you can use to create a responsive web application.
 
+media query 
+bootstrap 
+flexbox and grid 
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
 
 Q9) What is the difference between authentication and authorization?
+authentication : 
+authorization : who have the token to get the information 
 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
+
+to save the secret information and to keep it save from sharing with pepole.
 
 */
