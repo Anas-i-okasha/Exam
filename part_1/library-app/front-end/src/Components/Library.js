3e4b7233a770d843d59a3c90d2bd7f3c4cf1d95e
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-01-01 23:22:54 +0200

    solve q7

--- part_1/library-app/front-end/src/Components/Library.js
+++ part_1/library-app/front-end/src/Components/Library.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React ,  {state} from 'react'
 import Axios from 'axios'
 
-const Library=()=>
-Axios.get().then(()=>{
-    
-}).err(())
+const Library=()=> {
+    const [books , setBooks]=[]
+    const [title , setTile]=("")
+    const [author , setAuthor]=('')
+    const [publisher , setPublisher]=('')
+    const [publisher_at , setPublesher_at]=('')
 
- {
+   const getAll=()=>{
+    Axios.get('http://localhost:3000').then((responce)=>{
+
+    })
+    .err((err)=>{
+        console.log(err)
+    })
+   } 
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <div>
 
-export default Library
+   {books.map((item)=>{ return <h1>Title : {item.title}</h1> , <h1> Author:{item.author} </h1>  , <h1>Publisher : {item.publisher}</h1> })}
+           <button onClick={getAll}></button>
+      </div>
+    )
+  }
+  export default Library
+
